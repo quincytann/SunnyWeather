cc6cc7812e92d3ff5430af4242b10e9e6271d641
tanqing <tanqing.ink@bytedance.com>
2020-10-06 22:39:55 +0800

    展示天气信息到主界面

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
@@ -1,5 +1,7 @@
 package com.sunnyweather.android.ui.place
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.ui.weather.WeatherActivity
 
 
 class PlaceAdapter(private val frament: Fragment, private val placeList: List<Place>) :
@@ -20,7 +23,21 @@ class PlaceAdapter(private val frament: Fragment, private val placeList: List<Pl
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val place = placeList[position]
+            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                putExtra("location_lng",place.location.lng)
+                putExtra("location_lat",place.location.lat)
+                putExtra("place_name",place.name)
+            }
+            Log.d("123","--------*********")
+            frament.startActivity(intent)
+            Log.d("123","--------/////////")
+        }
+
+        return holder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -2,11 +2,89 @@ package com.sunnyweather.android.ui.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.sunnyweather.android.R
+import com.sunnyweather.android.logic.model.Weather
+import com.sunnyweather.android.logic.model.getSky
+import kotlinx.android.synthetic.main.activity_weather.*
+import kotlinx.android.synthetic.main.forecast.*
+import kotlinx.android.synthetic.main.forecast_item.*
+import kotlinx.android.synthetic.main.life_index.*
+import kotlinx.android.synthetic.main.now.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
+
+    val viewModel by lazy { ViewModelProviders.of(this).get(WeatherViewModel::class.java) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        if(viewModel.locationLng.isEmpty()) {
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+        if(viewModel.locationLat.isEmpty()) {
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+        if(viewModel.placeName.isEmpty()) {
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+        viewModel.weatherLiveData.observe(this, Observer { result ->
+            val weather = result.getOrNull()
+            if(weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "无法成功获得天气信息", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+        viewModel.refreshWeather(viewModel.locationLng,viewModel.locationLat)
     }
+
+    private fun showWeatherInfo(weather: Weather) {
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        // 填充now.xml
+        currentTemp.text = "${realtime.temperature.toInt()} ˚C"
+        currentSky.text = getSky(realtime.skycon).info
+        currentAQI.text  = "空气指数 ${realtime.airQuality.aqi.chn.toInt()}"
+        nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        // 填充forecast.xml
+        forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for (i in 0 until days) {
+            var skycon =  daily.skycon[i]
+            var temperature = daily.temperature[i]
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
+                forecastLayout,false)
+            val dateInfo = view.findViewById(R.id.dateInfo) as TextView
+            val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
+            val skyInfo = view.findViewById(R.id.skyInfo) as TextView
+            val tempratureInfo = view.findViewById(R.id.tempratureInfo) as TextView
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dateInfo.text = simpleDateFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            skyIcon.setImageResource(sky.icon)
+            skyInfo.text = sky.info
+            tempratureInfo.text = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} ˚C"
+            forecastLayout.addView(view)
+        }
+        // 填充 life_index.xml
+        val lifeIndex = daily.lifeIndex
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        weatherLayout.visibility = View.VISIBLE
+    }
+
 }
